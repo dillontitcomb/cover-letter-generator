a5d98c4ccdfd1077122231182a693726cb6dd738
Dillon Titcomb <dillontitcomb@gmail.com>
2018-12-08 22:27:13 -0800

    fix input tag bug and add address input.

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ class App extends Component {
     console.log([e.target.name]);
     console.log(e.target.value);
 
-    this.setState({ [e.target.value]: e.target.value });
+    this.setState({ [e.target.name]: e.target.value });
   }
 
   render() {
